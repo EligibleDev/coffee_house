be7467e50b96ba83bb670f88ff0a4c00acbfc29b
EligibleDev <eligibledev@gmail.com>
2024-01-25 11:06:46 +0600

    primary header done for all devices and nav icons added inside home header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,9 @@ import Headroom from "react-headroom";
 import logo from "../../assets/images/logo.png";
 import useUtils from "../../hooks/useUtils/useUtils";
 import { Link, NavLink } from "react-router-dom";
-import { FaPhoneVolume } from "react-icons/fa6";
+import { FaBars, FaPhoneVolume } from "react-icons/fa6";
 import useAuth from "../../hooks/useAuth/useAuth";
-import { Tooltip } from "keep-react";
+import { Navbar, Tooltip } from "keep-react";
 import { MdLogin } from "react-icons/md";
 import React from "react";
 
@@ -19,8 +19,8 @@ const Header = () => {
                     <Link to="/home">
                         <img className="w-24" src={logo} alt="Coffee House Logo" />
                     </Link>
-                    <nav className="flex justify-end items-center gap-10">
-                        <ul className="flex gap-10">
+                    <nav className="flex justify-end items-center gap-5 sm:gap-10">
+                        <ul className="hidden xl:flex gap-10">
                             {navLinks.map((item) => (
                                 <NavLink
                                     to={item.link}
@@ -32,7 +32,7 @@ const Header = () => {
                             ))}
                         </ul>
 
-                        <div className="border-x border-x-gray-700 px-8 py-3">
+                        <div className="hidden sm:block border-r xl:border-x border-x-gray-700 px-8 py-3">
                             <a
                                 href="tel:+8801619152852"
                                 className="flex justify-between items-center gap-4 text-white hover:text-primary-green transition font-bold text-sm"
@@ -43,6 +43,15 @@ const Header = () => {
                         </div>
 
                         <div className="flex gap-5">
+                            {/* {headerIcons?.map(({ icon, link, tooltip }, index) => (
+                                <Tooltip content={tooltip} style="light" key={index}>
+                                    <Link to={link}>
+                                        {React.createElement(icon, {
+                                            className: iconClasses,
+                                        })}
+                                    </Link>
+                                </Tooltip>
+                            ))} */}
                             {user ? (
                                 headerIcons?.map(({ icon, link, tooltip }, index) => (
                                     <Tooltip content={tooltip} style="light" key={index}>
@@ -61,6 +70,45 @@ const Header = () => {
                                 </Tooltip>
                             )}
                         </div>
+
+                        <Navbar className="block xl:hidden text-white bg-transparent p-0">
+                            <Navbar.Toggle
+                                openIcon={<FaBars size={30} />}
+                                className="block"
+                            ></Navbar.Toggle>
+
+                            <Navbar.Collapse
+                                collapseType="sidebar"
+                                className="fixed right-0 top-0 bg-white p-10 !w-full !h-screen"
+                            >
+                                <Navbar.Container
+                                    tag="nav"
+                                    className="flex flex-col justify-between h-full text-secondary-black"
+                                >
+                                    <ul className="flex flex-col gap-5">
+                                        {navLinks?.map((item) => (
+                                            <NavLink
+                                                className="text-sm font-bold"
+                                                key={item?.link}
+                                                to={item?.link}
+                                            >
+                                                {item?.label}
+                                            </NavLink>
+                                        ))}
+                                    </ul>
+
+                                    <div className="pb-10">
+                                        <a
+                                            href="tel:+8801619152852"
+                                            className="flex justify-start items-center gap-4 hover:text-primary-green transition font-bold text-sm"
+                                        >
+                                            <FaPhoneVolume className="text-primary-green text-xl" />
+                                            <p>0905-456-987-3</p>
+                                        </a>
+                                    </div>
+                                </Navbar.Container>
+                            </Navbar.Collapse>
+                        </Navbar>
                     </nav>
                 </div>
             </header>
