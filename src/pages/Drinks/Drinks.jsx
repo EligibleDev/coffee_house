a7d227d282c29e2078ceacb0d76faa1c154c816f
EligibleDev <eligibledev@gmail.com>
2024-01-26 07:31:49 +0600

    sortbar init

--- src/pages/Drinks/Drinks.jsx
+++ src/pages/Drinks/Drinks.jsx
@@ -1,5 +1,6 @@
 import PageCover from "../../components/PageCover/PageCover";
 import Sidebar from "./components/Sidebar/Sidebar";
+import SortBar from "./components/SortBar/SortBar";
 
 const Drinks = () => {
     return (
@@ -7,6 +8,9 @@ const Drinks = () => {
             <PageCover title="All Drinks" subTitle="Premium quality drinks" />
             <div className="container mx-auto flex justify-between gap-10 py-20">
                 <Sidebar />
+                <div className="w-3/4">
+                    <SortBar/>
+                </div>
             </div>
         </>
     );
---
+++ src/pages/Drinks/components/SortBar/SortBar.jsx
@@ -0,0 +1,11 @@
+const SortBar = () => {
+      // todo: https://react.keepdesign.io/docs/components/dropdown
+
+    return (
+        <div className="w-full bg-[#efece5] flex justify-between items-center py-4 px-7">
+            <p className="font-jost text-secondary-black text-sm">Showing 1-6 of 19 results</p>
+        </div>
+    );
+};
+
+export default SortBar;
