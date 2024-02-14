c9a82c798fa2eb2f2cce202409bec772df669194
EligibleDev <eligibledev@gmail.com>
2024-02-14 20:14:09 +0600

    product card edited

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -3,19 +3,14 @@ import "./card.css";
 
 const ProductCard = ({ product }) => {
     return (
-        <figure
-            className="card relative bg-[#efece5] w-full p-3 transition-all duration-75"
-            key={product?._id}
-        >
-            <div className="">
-                <img className="w-52 mx-auto" src={product?.thumbnail} alt="" />
-            </div>
+        <div className="card h-64">
+            <img className="w-full" src={product.image} alt={product.name} />
 
-            <div className="lower-part h-2/6 w-full relative">
-                <div className="lower-part-face text-primary-green"> {product?._id}</div>
-                <div className="lower--part-back">cart</div>
+            <div className="lower-part w-full">
+                <div className="lower-part-face w-full h-full bg-white">{product.name}</div>
+                <div className="lower-part-back w-full h-full bg-white">{product._id}</div>
             </div>
-        </figure>
+        </div>
     );
 };
 
